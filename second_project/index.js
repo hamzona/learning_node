7b7ed2cd58fc7a33b9ee15b61d06c2b4a61b206f
hamzona <hamzadjedovic2000@gmail.com>
2024-09-11 10:03:50 -0700

    many things

--- second_project/index.js
+++ second_project/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { v4 } = require("uuid");
 
 const app = express();
+
+const databasePath = path.join(__dirname, "data", "products.json");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -10,32 +13,52 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => res.redirect("/products"));
+
+app.get("/products", (req, res) => {
 	const products = fs.readFile(
 		path.join(__dirname, "data", "products.json"),
 		(err, products) => {
 			res.render("index", {
-				pageTitle: "nesto",
-				title: "Hamza",
+				pageTitle: "Home page",
+				title: "Home",
 				products: JSON.parse(products),
 			});
 		},
 	);
 });
 
-app.get("/addProduct", (req, res) => {
-	res.render("addProdukt", { pageTitle: "nesto drugo", title: "add produkt" });
+app.get("/product/:id", (req, res) => {
+	const { id } = req.params;
+
+	console.log(id);
+
+	fs.readFile(databasePath, (err, products) => {
+		const product = JSON.parse(products).find((product) => product.id === id);
+
+		console.log(product);
+		res.render("product-datails", { pageTitle: product.title, product });
+	});
 });
 
-app.post("/addProduct", (req, res) => {
+app.get("/admin/addProduct", (req, res) => {
+	res.render("addProdukt", {
+		pageTitle: "Add new product",
+		title: "Add produkt",
+	});
+});
+
+app.post("/admin/addProduct", (req, res) => {
 	const { title, price } = req.body;
-
-	const databasePath = path.join(__dirname, "data", "products.json");
-
+	const product = {
+		id: v4(),
+		title,
+		price,
+	};
 	fs.readFile(databasePath, (err, products) => {
 		console.log(JSON.parse(products));
 
-		const updatedProducts = [...JSON.parse(products), { title, price }];
+		const updatedProducts = [...JSON.parse(products), product];
 
 		fs.writeFile(databasePath, JSON.stringify(updatedProducts), () => {
 			res.redirect("/");
